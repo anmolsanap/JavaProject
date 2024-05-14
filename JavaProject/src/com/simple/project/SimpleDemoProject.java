a029d515d378b63cc0e555c14875cbdf64b97386
anmolsanap <158318618+anmolsanap@users.noreply.github.com>
2024-05-14 17:20:00 +0530

    Update SimpleDemoProject.java
    display method is added

--- JavaProject/src/com/simple/project/SimpleDemoProject.java
+++ JavaProject/src/com/simple/project/SimpleDemoProject.java
@@ -5,6 +5,9 @@ public class SimpleDemoProject {
 	public void name() {
 		System.out.println("name method");
 	}
+	public void display() {
+		System.out.println("display method");
+	}
 	public static void main(String[] args) {
 		System.out.println("main method started");
 	}
